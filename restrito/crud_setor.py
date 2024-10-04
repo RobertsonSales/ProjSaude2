b4831e72d5031c1e6058314ecfb62d6d4411c9f1
RobertsonSales <robertguima@gmail.com>
2024-10-04 17:13:34 -0300

    Update crud_setor.py

--- restrito/crud_setor.py
+++ restrito/crud_setor.py
@@ -191,7 +191,7 @@ def show_setores_crud():
                     unidade_names = [f"{unidade['id_unidade']} - {unidade['nome_unidade']}" for unidade in unidades]
                     selected_unidade = st.selectbox("Selecione a Unidade para listar os Setores", unidade_names)
                     selected_unidade_id = int(selected_unidade.split(' - ')[0])
-                    n_unid = selected_unidade.strip[1]
+                    n_unid = selected_unidade.strip([1])
 
                     setores = get_setores_by_unidade(selected_unidade_id)
                     
