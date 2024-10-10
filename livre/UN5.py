19541ec3ae7f363b4f21968e606ba89c2713885f
RobertsonSales <robertguima@gmail.com>
2024-10-10 15:39:59 -0300

    Update UN5.py

--- livre/UN5.py
+++ livre/UN5.py
@@ -51,9 +51,9 @@ def Limp():
 
 # Função para exibir a lista e gerar o PDF
 def List():
-    st.success("Clínicas da Família no Rio de Janeiro - Você pode obter mais informações clicando em 'Ir à página'.")
+    st.success("Policlínicas no Rio de Janeiro - Você pode obter mais informações clicando em 'Ir à página'.")
     if st.button('Imprimir lista'):
-        base = "cli_fam_rio"
+        base = "polic_rio"
         dados = Querys.Select_Frame(base)
         colunas = ['Unidade', 'Tipo', 'Endereço', 'Bairro', 'Telefone', 'Funcionamento']
         df = pd.DataFrame(dados, columns=colunas)
